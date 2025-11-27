733a686a52c9ecf472255027119b7d65b2bab007
Carlota <carloproh@gmail.com>
2025-11-27 12:49:48 +0100

    Bd

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
 import pandas as pd
 import pickle
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
 with open("model.pkl", "rb") as archivo:
     modelo = pickle.load(archivo) # load para leer el archivo
+
+churro = 'postgresql://mypostgres_njxk_user:66fLtLNl757NYqK3rlPTTU2svjffZjmL@dpg-d4k3b563jp1c738ieiag-a.ohio-postgres.render.com/mypostgres_njxk'
+engine = create_engine(churro)
 
 @app.route("/", methods=["GET"])
 def hola():
@@ -18,7 +22,7 @@ def predict():
     clase = request.args.get("clase", None)
 
     if age is None or sex is None or clase is None:
-        return "No se ha encontrado el id"
+        return "No se ha encontrado los parametros"
     
     if not (str(age).isnumeric() or str(sex).isnumeric() or str(clase).isnumeric()):
         return """<h1>Tiene que ser un número <h1 style="color:red">¡melón!<h1>"""
@@ -28,6 +32,10 @@ def predict():
     clase = int(clase)
 
     survived = "Sobrevivio" if modelo.predict([[age, sex, clase]])[0] else "Palmo"
+
+    df = pd.DataFrame({"sex": [sex], "age":[age], "class": [clase], "prediction":[survived]})
+    df.to_sql("predictions", con=engine, if_exists="append", index=None)
+
     return f"""Para la persona con {age} años, sexo {sex} y clase {clase}, la prediccion es {survived}"""
 
 if __name__ == "__main__":
